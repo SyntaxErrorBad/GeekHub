3681b8e033f35c0c06dc7375d8de4baeb68a1cb7
Vitali <130182133+SyntaxErrorBad@users.noreply.github.com>
2023-10-19 19:09:22 +0300

    Delete task_3.py

--- task_3.py
+++
@@ -1,6 +0,0 @@
-# HT_02
-# 3. Write a script which accepts a <number> from user and print out a sum of the first <number> positive integers.
-
-number = int(input("Введіть число: "))
-sum_of_integers = sum(range(1, number + 1))
-print("Сума перших", number, "додатніх цілих чисел:", sum_of_integers)
