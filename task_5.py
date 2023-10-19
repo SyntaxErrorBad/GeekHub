c4a2d1c5785da4c52e53031b468a75b2c2da80e3
Vitali <130182133+SyntaxErrorBad@users.noreply.github.com>
2023-10-19 19:09:37 +0300

    Delete task_5.py

--- task_5.py
+++
@@ -1,4 +0,0 @@
-# HT_02
-# 5. Write a script which accepts decimal number from user and converts it to hexadecimal.
-
-print("Шістнадцяткове представлення:", hex(int(input("Введіть десяткове число: "))))
