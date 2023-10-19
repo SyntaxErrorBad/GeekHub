a8520deb28e097d1ed4682b0b041a56bd3fb8c61
Vitali <130182133+SyntaxErrorBad@users.noreply.github.com>
2023-10-19 19:08:34 +0300

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,6 @@
+# HT_02
+# 1. Write a script which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.
+
+
+numbers = (input("Numbers ',': ")).split(",")
+print(numbers,tuple(numbers))
---
+++ task_7.py
@@ -0,0 +1,5 @@
+# HT_02
+# 7. Write a script to concatenate all elements in a list into a string and print it. List must be include both strings and integers and must be hardcoded.
+
+my_list = [1, 'два', 3, 'чотири', 5]
+print("Об'єднаний рядок:", ''.join(map(str, my_list)))
