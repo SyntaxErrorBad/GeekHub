6414a231a76707792e365e04d527e3310f6227d0
Vitali <130182133+SyntaxErrorBad@users.noreply.github.com>
2023-10-19 19:10:21 +0300

    Delete task_7.py

--- task_7.py
+++
@@ -1,5 +0,0 @@
-# HT_02
-# 7. Write a script to concatenate all elements in a list into a string and print it. List must be include both strings and integers and must be hardcoded.
-
-my_list = [1, 'два', 3, 'чотири', 5]
-print("Об'єднаний рядок:", ''.join(map(str, my_list)))
