c74e8e958260d00250eda5a7b068275bfb5c8faf
Vitali <130182133+SyntaxErrorBad@users.noreply.github.com>
2023-10-19 19:09:03 +0300

    Delete task_1.py

--- task_1.py
+++
@@ -1,6 +0,0 @@
-# HT_02
-# 1. Write a script which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.
-
-
-numbers = (input("Numbers ',': ")).split(",")
-print(numbers,tuple(numbers))
