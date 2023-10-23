1693cf801896d56ef24452337d1a72757c0a4851
Vitali <130182133+SyntaxErrorBad@users.noreply.github.com>
2023-10-23 18:13:33 +0300

    Update task_6.py

--- HT_02/task_6.py
+++ HT_02/task_6.py
@@ -1,6 +1,37 @@
 # HT_02
 # 6. Write a script to check whether a value from user input is contained in a group of values.    e.g. [1, 2, 'u', 'a', 4, True] --> 2 --> True
 #          [1, 2, 'u', 'a', 4, True] --> 5 --> False
+# HT_02
+# 6. Write a script to check whether a value from user input is contained in a group of values.    e.g. [1, 2, 'u', 'a', 4, True] --> 2 --> True
+#          [1, 2, 'u', 'a', 4, True] --> 5 --> False
+
 m_list = ["hello",2,True,3.2,"nice"]
-item = input("Item: ")
-print("Чи міститься значення у списку:", item in m_list)
+
+#Варіант 1
+def type_input(item):
+    if item.isdigit():
+        return float(item)
+    elif item == "True":
+        return True
+    elif item == "False":
+        return False
+    else:
+        return str
+user = input("Введіть що перевірити:")
+print("Чи міститься значення у списку:", type_input(user) in m_list)
+
+#Вараінт 2
+def choose_type(ch_type):
+    if ch_type == "bool":
+        return bool(input("Введіть що перевірити:"))
+    elif ch_type == "int":
+        return int(input("Введіть що перевірити:"))
+    elif ch_type == "float":
+        return float(input("Введіть що перевірити:"))
+    elif ch_type == "str":
+        return str(input("Введіть що перевірити:"))
+    else:
+        print("Нажаль такого немає")
+
+user = choose_type(input("Введіть тип яких хочете перевірити [str,int,float,bool]: "))
+print("Чи міститься значення у списку:", user in m_list)
