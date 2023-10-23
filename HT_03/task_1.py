33e3dd41be68ac6b74f899fd21adb338cb5e45bf
Vitali <130182133+SyntaxErrorBad@users.noreply.github.com>
2023-10-23 20:46:01 +0300

    Update task_1.py

--- HT_03/task_1.py
+++ HT_03/task_1.py
@@ -2,12 +2,15 @@
 #1. Write a script that will run through a list of tuples and replace the last value for each tuple. 
 #The list of tuples can be hardcoded. The "replacement" value is entered by user. The number of elements in the tuples must be different.
 
-tuples = [(1, 2, 3), (4, 5), (6, 7, 8, 9)]
+tuples = [(1, 2, 3), (4, 5), (6, 7, 8, 9),()]
 item = int(input("Число: "))
 new_tuple_list = []
 for i in tuples:
-    tup = list(i)
-    tup[-1] = item
-    new_tuple_list.append(tuple(tup)) 
+    if len(i) != 0:
+        tup = list(i)
+        tup[-1] = item
+        new_tuple_list.append(tuple(tup)) 
+    else:
+        new_tuple_list.append(i)
 
 print(new_tuple_list)
