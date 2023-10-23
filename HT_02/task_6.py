c95b182b2c063f9528cf62ef5928832a79c18991
Vitali <130182133+SyntaxErrorBad@users.noreply.github.com>
2023-10-23 12:16:29 +0300

    Update task_6.py

--- HT_02/task_6.py
+++ HT_02/task_6.py
@@ -2,5 +2,5 @@
 # 6. Write a script to check whether a value from user input is contained in a group of values.    e.g. [1, 2, 'u', 'a', 4, True] --> 2 --> True
 #          [1, 2, 'u', 'a', 4, True] --> 5 --> False
 m_list = ["hello",2,True,3.2,"nice"]
-item = 3.2
+item = input("Item: ")
 print("Чи міститься значення у списку:", item in m_list)
