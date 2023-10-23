18a21c65428084028d9f2dd8e96442eb02b8e673
Vitali <130182133+SyntaxErrorBad@users.noreply.github.com>
2023-10-23 21:49:30 +0300

    Update task_6.py

--- HT_02/task_6.py
+++ HT_02/task_6.py
@@ -3,40 +3,5 @@
 #          [1, 2, 'u', 'a', 4, True] --> 5 --> False
 
 m_list = ["hello",2,True,3.2,"nice"]
-#Варіант 1
-def type_input(item):
-    if item.isdigit():
-        return float(item)
-    elif item == "True":
-        return True
-    elif item == "False":
-        return False
-    else:
-        return str
 user = input("Введіть що перевірити:")
-print("Чи міститься значення у списку:", type_input(user) in m_list)
-
-#Вараінт 2
-
-def choose_type(ch_type):
-    if ch_type == "bool":
-        return bool(input("Введіть що перевірити:"))
-    elif ch_type == "int":
-        return int(input("Введіть що перевірити:"))
-    elif ch_type == "float":
-        return float(input("Введіть що перевірити:"))
-    elif ch_type == "str":
-        return str(input("Введіть що перевірити:"))
-    elif ch_type == "list":
-        return list(input("Введіть що перевірити:"))
-    elif ch_type == "set":
-        return set(input("Введіть що перевірити:"))
-    elif ch_type == "tuple":
-        return tuple(input("Введіть що перевірити:"))
-    elif ch_type == "complex":
-        return complex(input("Введіть що перевірити:"))
-    else:
-        print("Нажаль такого немає")
-
-user = choose_type(input("Введіть тип яких хочете перевірити [str,int,float,bool,list,tuple,complex]: "))
-print("Чи міститься значення у списку:", user in m_list)
+print("Чи міститься значення у списку:", user in [str(item) for item in m_list])
