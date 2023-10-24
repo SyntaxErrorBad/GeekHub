4784e318894f61df65462e7420b9ea9a19d3588c
Vitali <130182133+SyntaxErrorBad@users.noreply.github.com>
2023-10-24 18:33:09 +0300

    Update task_6.py

--- HT_03/task_6.py
+++ HT_03/task_6.py
@@ -1,4 +1,4 @@
 #HT_03
 #Write a script to get the maximum and minimum value in a dictionary.
 my_dict = {'a': 10, 'b': 5, 'c': 15, 'd': [20], 'e': 5}
-print("Min {}, Max {}".format(min(value for value in my_dict.values() if not isinstance(value, list)),max(value for value in my_dict.values() if not isinstance(value, list))))
+print("Min {}, Max {}".format(min(value for value in my_dict.values() if value.isdigit(),max(value for value in my_dict.values() value.isdigit())))
