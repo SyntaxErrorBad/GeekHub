64d1c85abaa890d07344f7895c72e91845ad833a
Vitali <130182133+SyntaxErrorBad@users.noreply.github.com>
2023-11-01 23:43:08 +0200

    Update task_6.py

--- HT_06/task_6.py
+++ HT_06/task_6.py
@@ -5,9 +5,22 @@
 #    fnc([1, 2, 3, 4, 5], shift=1) --> [5, 1, 2, 3, 4]
 #    fnc([1, 2, 3, 4, 5], shift=-2) --> [3, 4, 5, 1, 2]
 
+def check_len(length,lst):
+    if length < 0:
+        if length*-1 <= len(lst):
+            return True
+    elif length <= len(lst):
+        return True
+    else:
+        return False
+
+
 def working_with_list(lst, shift):
-    shifted_list = lst[-shift:] + lst[:-shift]
-    return shifted_list
+    if check_len(shift,lst):
+        shifted_list = lst[-shift:] + lst[:-shift]
+        return shifted_list
+    else:
+        return "Shift занадто великий"
 
 
 print(working_with_list([1, 2, 3, 4, 5], -2))
