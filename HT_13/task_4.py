5af5ecb9d4dc254b6a4861b155c681eeccd9f5a1
Vitali <130182133+SyntaxErrorBad@users.noreply.github.com>
2023-12-02 21:36:21 +0200

    Update task_4.py

--- HT_13/task_4.py
+++ HT_13/task_4.py
@@ -2,100 +2,57 @@
 # Тобто це повинен бути клас, який буде поводити себе так, як list (маючи основні методи),
 # але індексація повинна починатись із 1
 
-class CustomList:
-    def __init__(self,data = []):
-        self._data = data
-
-    def __getitem__(self, index):
-        if isinstance(index, int):
-            if index == 0:
-                raise IndexError("Index starts from 1")
-            if index < 0:
-                index = len(self._data) + index + 1
-            return self._data[index - 1]
-        else:
-            return self._data[index]
-
-    def __setitem__(self, index, value):
-        if isinstance(index, int):
-            if index == 0:
-                raise IndexError("Index starts from 1")
-            if index < 0:
-                index = len(self._data) + index + 1
-            if index > len(self._data):
-                self._data.extend([None] * (index - len(self._data)))
-            self._data[index - 1] = value
-        else:
-            self._data[index] = value
-
-    def __delitem__(self, index):
-        if isinstance(index, int):
-            if index == 0:
-                raise IndexError("Index starts from 1")
-            if index < 0:
-                index = len(self._data) + index + 1
-            del self._data[index - 1]
-        else:
-            del self._data[index]
-    
-    def __len__(self):
-        return len(self._data)
-
-    def append(self, value):
-        self._data.append(value)
-
-    def extend(self, values):
-        self._data.extend(values)
-
-    def remove(self, value):
-        self._data.remove(value)
-
-    def pop(self, index=-1):
-        if index == 0:
+class NewCustom(list):
+    def adjust_index(self, key):
+        if key >= 1:
+            return key - 1
+        elif key == 0:
             raise IndexError("Index starts from 1")
-        return self._data.pop(index - 1)
-
-    def insert(self, index, value):
-        if index == 0:
-            raise IndexError("Index starts from 1")
-        self._data.insert(index - 1, value)
-
-    def index(self, value, start=1, stop=None):
-        return self._data.index(value, start - 1 if start else start, stop)
-
-    def count(self, value):
-        return self._data.count(value)
-
-    def clear(self):
-        self._data.clear()
-    
-    def __iter__(self):
-        self._iter_index = 0
-        return self
-
-    def __next__(self):
-        if self._iter_index < len(self._data):
-            result = self._data[self._iter_index]
-            self._iter_index += 1
-            return result
         else:
-            raise StopIteration
+            return key
 
-    def __repr__(self):
-        return repr(self._data)
+    def __getitem__(self, key):
+        key = self.adjust_index(key)
+        return super(NewCustom, self).__getitem__(key)
+    
+    def __setitem__(self, key,value):
+        key = self.adjust_index(key)
+        return super(NewCustom, self).__setitem__(key,value)
+
+    def __delitem__(self, key):
+        key = self.adjust_index(key)
+        return super(NewCustom, self).__delitem__(key)
+
+    def index(self, value):
+        return (super(NewCustom, self).index(value))+1
+    
+    def pop(self, key):
+        return super(NewCustom, self).pop(key - 1)
+
+    def insert(self, key, element):
+        return super(NewCustom, self).insert(key-1,element)
     
     
 
-custom_list = CustomList()
+custom_list = NewCustom()
 custom_list.append('a')
+custom_list.append('b')
+print(custom_list)
+custom_list.reverse()
 custom_list.extend(['b', 'c',3])
 print(custom_list)
 print(len(custom_list))
-
-# Операції індексації
 print(custom_list[1])
-print(custom_list[2])
 print(custom_list[-1])
-print(custom_list[1])
+print(custom_list.index('b'))
+custom_list.pop(-1)
+print(custom_list)
+custom_list.insert(2,"c")
+print(custom_list)
+custom_list[1] = "g"
+print(custom_list)
+print(custom_list.count("b"))
+del custom_list[1]
+print(custom_list)
 for i in custom_list:
-    print(i)    
+    print(i)       
