fd200a27bf84d8257f9a9c4eb0d2620623b53553
Vitali <130182133+SyntaxErrorBad@users.noreply.github.com>
2024-01-15 19:19:24 +0200

    Delete HT_19/scrapingsite directory

--- HT_19/scrapingsite/manageproducts/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class AddProductsForm(forms.Form):
-    product_ids = forms.CharField(widget=forms.Textarea(attrs={'rows': 5, 'cols': 20}))
-
-
-class ShoppingCartItemForm(forms.Form):
-    quantity = forms.IntegerField(min_value=1, label='Counts')
