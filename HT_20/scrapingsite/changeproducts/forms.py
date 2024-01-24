c157076fdc112270a20fbfb607c4cb2e9202495c
Vitali <130182133+SyntaxErrorBad@users.noreply.github.com>
2024-01-24 17:56:25 +0200

    Delete HT_20/scrapingsite directory

--- HT_20/scrapingsite/changeproducts/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from manageproducts.models import Product
-
-
-class ProductEditForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = ['name', 'price', 'description', 'brand', 'category', 'sears_link', 'img']
--- HT_20/scrapingsite/changeproducts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('changeinfoproduct/<str:product_id>/', views.change_info_product, name='changeinfoproduct'),
-]
